d035c125cc99ef4375dec07e23a0e397fb942eaf
Pushti Prajapati <pushti.prajapati@reynasolutions.com>
2023-08-16 12:35:46 +0530

    PP-create

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductInformationDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
